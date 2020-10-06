acc0577b040a98cdb32e50c01bfe29680995245e
tiulimg <tiulimg@gmail.com>
2020-10-07 01:25:36 +0300

    connect mongoose

--- services/dbservices.js
+++ services/dbservices.js
@@ -13,6 +13,5 @@ function initialize(app) {
     var server = app.listen(process.env.PORT || 8080, function () {
         var port = server.address().port;
         console.log("App now running on port", port);
-        return resolve();
     });
 }
