29348ea648130508df76b00d7986b0ced21751bc
tiulimg <tiulimg@gmail.com>
2021-07-18 17:58:30 +0300

    don't log emails twice

--- mail.js
+++ mail.js
@@ -10,7 +10,7 @@ function emailyoung(recipient, matureemail, youngname1, youngname2, maturename1,
     var mailbody = `היי ${youngname1} ו-${youngname2} 🙂 מה שלומכם?
     
     ${maturename1} ו-${maturename2} ישמחו להקשיב לכם ולתת לכם עצות מנסיונם בנוגע ליום יום הזוגי. נראה שתמצאו הרבה במשותף.
-    תוכלו ליצור איתם קשר באימייל ${matureemail}, העברנו להם גם את האימייל שלכם. בהצלחה רבה! 👍🏽
+    תוכלו ליצור איתם קשר באימייל ${matureemail}, העברנו להם גם את האימייל שלכם. בהצלחה רבה! 👍🏼
 
     ברגע שנמצאה התאמה, הסרנו את הפרופיל שלכם מהמערכת שלנו כדי שתראו אם הליווי הזה עובד. אם תרגישו צורך לנסות שוב תוכלו בכל כעת להרשם שוב בטופס שלנו https://docs.google.com/forms/d/1pNgAZGTjb-bFJeBNLh5K4iZyu5jwydUbAzSpPIr6bAE/viewform?edit_requested=true
     עדכנו אותנו אם הצלחתם ליצור חיבור טוב איתם ואם החיבור איתם תרם לכם. 
@@ -29,9 +29,6 @@ function emailyoung(recipient, matureemail, youngname1, youngname2, maturename1,
 
     sgMail
     .send(data)
-    .then(() => {
-        console.log('Email sent')
-    })
     .catch((error) => {
         console.error(error)
     })
@@ -44,7 +41,7 @@ function emailmature(recipient, youngemail, youngname1, youngname2, maturename1,
     var mailbody = `היי ${maturename1} ו-${maturename2} 🙂 מה שלומכם?
     
     ${youngname1} ו-${youngname2} ישמחו לקבל מכם עצות מנסיונכם בנוגע ליום יום הזוגי. נראה שתמצאו הרבה במשותף.
-    כתובת האימייל שלהם היא ${youngemail}, העברנו להם גם את האימייל שלכם והם יצרו אתכם קשר בזמנם החופשי ולפי הרגשת הלב שלהם, אין צורך שתכתבו להם ביוזמתכם. כל הכבוד לכם על ההתנדבות ובהצלחה רבה! 👍🏽
+    כתובת האימייל שלהם היא ${youngemail}, העברנו להם גם את האימייל שלכם והם יצרו אתכם קשר בזמנם החופשי ולפי הרגשת הלב שלהם, אין צורך שתכתבו להם ביוזמתכם. כל הכבוד לכם על ההתנדבות ובהצלחה רבה! 👍🏼
 
     עדכנו אותנו אם הצלחתם ליצור חיבור טוב איתם ואם החיבור איתם תרם להם או לכם. 
     
@@ -62,9 +59,6 @@ function emailmature(recipient, youngemail, youngname1, youngname2, maturename1,
 
     sgMail
     .send(data)
-    .then(() => {
-        console.log('Email sent')
-    })
     .catch((error) => {
         console.error(error)
     })
